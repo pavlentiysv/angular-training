7648a8ef144a7d4bc0a2af23c0071113b345ab64
p-sviridov <pavel.sviridov@hiqo-solutions.com>
2019-09-06 17:57:49 +0300

    Displaying a list

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Axe' },
+  { id: 12, name: 'Faceless Void' },
+  { id: 13, name: 'Windranger' },
+  { id: 14, name: 'Legion Commander' },
+  { id: 15, name: 'Crystal Maiden' },
+  { id: 16, name: 'Denis Loh' },
+  { id: 17, name: 'Wraith King' },
+  { id: 18, name: 'Lifestealer' },
+  { id: 19, name: 'Pugna' },
+  { id: 20, name: 'Troll Warlord' }
+];
